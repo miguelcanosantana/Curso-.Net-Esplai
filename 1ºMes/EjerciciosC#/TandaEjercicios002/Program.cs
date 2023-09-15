890276d9b4f281e3d0f22e87e285b71e6ccc6c25
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-09-16 01:35:44 +0200

    Update Program.cs

--- 1ºMes/EjerciciosC#/TandaEjercicios002/Program.cs
+++ 1ºMes/EjerciciosC#/TandaEjercicios002/Program.cs
@@ -16,7 +16,7 @@ namespace TandaEjercicios002
 
             Console.Title = "Choose an exercise :)";
 
-            Print("Choose an Exercise (Enter a number from 1 to 14): ");
+            Print("Choose an Exercise (Enter a number from 1 to 15): ");
             exerciseToDisplay = Convert.ToInt32(Console.ReadLine());
 
             LaunchExercise(exerciseToDisplay);
@@ -93,11 +93,11 @@ namespace TandaEjercicios002
                     break;
 
                 case 14:
-
+                    CalculateIVA();
                     break;
 
                 case 15:
-
+                    SalesManager();
                     break;
 
                 default:
@@ -272,121 +272,41 @@ namespace TandaEjercicios002
         }
 
 
-        //private static void NumbersDoublesAndChars()
-        //{
-        //    int n = 1;
-        //    double a = 2.5f;
-        //    char c = 'a';
+        private static void CalculateIVA()
+        {
+            Print("Enter a product price without IVA to calculate it: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double addedIVA = (price / 100) * 21;
+            double finalPrice = price + addedIVA;
 
-        //    PrintL("n variable = " + n);
-        //    PrintL("a variable = " + a);
-        //    PrintL("c variable = " + c.ToString());
-
-        //    PrintL("n + a = " + (n + a));
-        //    PrintL("a - n = " + (a - n));
-        //    PrintL("c value = " + Convert.ToInt32(c));
-        //}
+            PrintL("Final price with added IVA is " + finalPrice + " euros.");
+        }
 
 
-        //private static void TonsOfCalcs()
-        //{
-        //    int x, y;
-        //    double n, m;
+        private static void SalesManager()
+        {
+            Print("Enter the number of sales: ");
+            int salesNumber = int.Parse(Console.ReadLine());
 
-        //    Print("Write an int: ");
-        //    x = Convert.ToInt32(Console.ReadLine());
+            Dictionary<int, double> salesDictionary = new Dictionary<int, double>();
 
-        //    Print("Write another int: ");
-        //    y = Convert.ToInt32(Console.ReadLine());
+            for (int sale = 1; sale <= salesNumber; sale++)
+            {
+                Print("Enter the spent on sale " + sale + ": ");
+                double spent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                salesDictionary.Add(sale, spent);
+            }
 
-        //    //CultureInfo.InvariantCulture fixes incorrect parsing
-        //    //https://stackoverflow.com/questions/1354924/how-do-i-parse-a-string-with-a-decimal-point-to-a-double
-        //    Print("Write a double: ");
-        //    n = Convert.ToDouble(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            double totalSales = 0;
 
-        //    Print("Write another double: ");
-        //    m = Convert.ToDouble(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            foreach (var entry in salesDictionary)
+            {
+                totalSales += entry.Value;
 
-        //    PrintL(
-        //        "Your numbers are: x = " + x +
-        //        " | y = " + y +
-        //        " | n = " + n +
-        //        " | m = " + m);
-
-        //    PrintL("x + y = " + (x + y));
-        //    PrintL("x - y = " + (x - y));
-        //    PrintL("x * y = " + (x * y));
-        //    PrintL("x / y = " + (x / y));
-        //    PrintL("x % y = " + (x % y));
-
-        //    PrintL("n + m = " + (n + m));
-        //    PrintL("n - m = " + (n - m));
-        //    PrintL("n * m = " + (n * m));
-        //    PrintL("n / m = " + (n / m));
-        //    PrintL("n % m = " + (n % m));
-
-        //    PrintL("x + n = " + (x + n));
-        //    PrintL("y / m = " + (y / m));
-        //    PrintL("y % m = " + (y % m));
-
-        //    PrintL(
-        //        "2x = " + 2 * x +
-        //        " | 2y = " + 2 * y +
-        //        " | 2n = " + 2 * n +
-        //        " | 2m = " + 2 * m);
-        //    PrintL("x + y + n + m = " + (x + y + n + m));
-        //    PrintL("x * y * n * m = " + (x * y * n * m));
-        //}
-
-
-        //private static void IncrDecrDupl()
-        //{
-        //    int n;
-        //    int resultA;
-        //    int resultB;
-
-        //    Print("Write an int: ");
-        //    n = Convert.ToInt32(Console.ReadLine());
-
-        //    resultA = n + 77;
-        //    PrintL("n + 77 = " + resultA);
-
-        //    resultB = resultA - 3;
-        //    PrintL("First Result - 3 = " + resultB);
-
-        //    PrintL("Second Result * 2 = " + resultB * 2);
-        //}
-
-
-        //private static void FourVariables()
-        //{
-        //    int a;
-        //    int b;
-        //    int c;
-        //    int d;
-
-        //    Print("Write an int: ");
-        //    a = Convert.ToInt32(Console.ReadLine());
-
-        //    Print("Write another int: ");
-        //    b = Convert.ToInt32(Console.ReadLine());
-
-        //    Print("Another one: ");
-        //    c = Convert.ToInt32(Console.ReadLine());
-
-        //    Print("The last one *_*: ");
-        //    d = Convert.ToInt32(Console.ReadLine());
-
-        //    b = c;
-        //    c = a;
-        //    a = d;
-        //    d = b;
-
-        //    PrintL(
-        //        "Your numbers are: a = " + a +
-        //        " | b = " + b +
-        //        " | c = " + c +
-        //        " | d = " + b);
-        //}
+                PrintL("The sale " + entry.Key + " was: " + entry.Value + " euros.");
+            }
+            
+            PrintL("Final sales were " + totalSales + " euros.");
+        }
     }
 }
