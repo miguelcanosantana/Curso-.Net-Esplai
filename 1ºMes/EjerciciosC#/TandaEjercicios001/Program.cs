039ae26e149bdf7a6b989037571ecfc66d8b05e7
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-09-15 17:34:06 +0200

    Updated.

---
+++ 1ºMes/EjerciciosC#/TandaEjercicios001/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TandaEjercicios001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int exerciseToDisplay = 0;
+
+            exerciseToDisplay = Convert.ToInt32(Console.ReadLine());
+
+            LaunchExercise(exerciseToDisplay);
+        }
+
+        private static void LaunchExercise(int exerciseToDisplay)
+        {
+            switch (exerciseToDisplay)
+            {
+                case 1:
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.Title = "Exercise " + exerciseToDisplay.ToString();
+            Console.ReadKey();
+        }
+    }
+}
