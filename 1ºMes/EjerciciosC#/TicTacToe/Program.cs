652f7266907864bff28583ece1e9895d1556b9a6
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-09-21 16:40:28 +0200

    Updated.

--- 1ºMes/EjerciciosC#/TicTacToe/Program.cs
+++ 1ºMes/EjerciciosC#/TicTacToe/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            TicTacToeMatch();
+        }
+
+
+        private static void TicTacToeMatch()
+        {
+            //https://www.tutorialsteacher.com/csharp/csharp-multi-dimensional-array
+            string[,] grid2D = new string[3, 3]{
+                {".", ".", "."},
+                {".", ".", "."},
+                {".", ".", "."},
+            };
         }
     }
 }
