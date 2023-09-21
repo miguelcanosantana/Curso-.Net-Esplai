e2469f222ea284ee9b883e9ebe95885e5c90f0d7
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-09-21 17:48:17 +0200

    Update Program.cs

--- 1ºMes/EjerciciosC#/TicTacToe/Program.cs
+++ 1ºMes/EjerciciosC#/TicTacToe/Program.cs
@@ -94,17 +94,6 @@ namespace TicTacToe
         
         private static void RefreshGrid(string[,] grid2D)
         {
-            ////GetLength(0) is the 1st dimension, GetLength(1) is the 2nd
-            //for (int row = 0; row < grid2D.GetLength(0); row++)
-            //{
-            //    Console.WriteLine("");
-
-            //    for (int column = 0; column < grid2D.GetLength(1); column++)
-            //    {
-            //        string printedChar = grid2D[row, column];
-            //        Console.Write(printedChar);
-            //    }
-            //}
 
             Console.WriteLine(" _______________________\n" +
                                 "|       |       |       |\n" +
