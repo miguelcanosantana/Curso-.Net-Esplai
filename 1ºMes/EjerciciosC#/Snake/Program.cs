7ac30408193ce5bab12a23c66e6dc14feaf44e54
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-09-27 16:45:10 +0200

    Update Program.cs

--- 1ºMes/EjerciciosC#/Snake/Program.cs
+++ 1ºMes/EjerciciosC#/Snake/Program.cs
@@ -6,23 +6,19 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
+
     public enum eBoxType
     {
-        None,
         Empty,
-        SnakePart
-    }
-
-
-    public class Box
-    {
-        private eBoxType boxType = eBoxType.None;
+        SnakeBody,
+        SnakeHead
     }
 
 
     internal class Program
     {
-        static List<List<string>> grid2D = new List<List<string>>();
+        static List<List<eBoxType>> grid2D = new List<List<eBoxType>>();
+        static int sideSize = 10;
 
         static void Main(string[] args)
         {
@@ -33,12 +29,45 @@ namespace Snake
         private static void SnakeAllLogic()
         {
             CreateGrid();
+
+            Console.ReadKey();
         }
 
 
         private static void CreateGrid()
         {
             grid2D.Clear();
+
+            for (int r = 0; r < grid2D.Count; r++)
+                for (int c = 0; c < grid2D[r].Count; c++)
+                    grid2D[r][c] = eBoxType.Empty;
         }
+
+
+        private static void PrintGrid()
+        {
+            for (int r = 0; r < grid2D.Count; r++)
+            {
+                Console.WriteLine("");
+                for (int c = 0; c < grid2D[r].Count; c++)
+                {
+
+                    switch (grid2D[r][c])
+                    {
+                        case eBoxType.Empty:
+                            Console.Write("·");
+                            break;
+                        case eBoxType.SnakeBody:
+                            Console.Write("+");
+                            break;
+                        case eBoxType.SnakeHead:
+                            Console.Write("⟩");
+                            break;
+                    }
+                    
+                }
+            }
+        }
+
     }
 }
