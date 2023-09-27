5b892f94266b6593a04549ae2097b62f58a879ca
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-09-27 16:30:29 +0200

    Update Program.cs

--- 1ºMes/EjerciciosC#/Snake/Program.cs
+++ 1ºMes/EjerciciosC#/Snake/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
+    public enum eBoxType
+    {
+        None,
+        Empty,
+        SnakePart
+    }
+
+
+    public class Box
+    {
+        private eBoxType boxType = eBoxType.None;
+    }
+
+
     internal class Program
     {
+        static List<List<string>> grid2D = new List<List<string>>();
+
         static void Main(string[] args)
         {
+            SnakeAllLogic();
+        }
+
+
+        private static void SnakeAllLogic()
+        {
+            CreateGrid();
+        }
+
+
+        private static void CreateGrid()
+        {
+            grid2D.Clear();
         }
     }
 }
