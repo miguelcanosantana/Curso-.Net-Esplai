d04e0b9115caabbbc301eedecbea5c5e3918b223
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-02 14:37:54 +0200

    Update Program.cs

--- 1ºMes/EjerciciosC#/CardsBattleGame/Program.cs
+++ 1ºMes/EjerciciosC#/CardsBattleGame/Program.cs
@@ -4,8 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CardsBattleGame
 {
+
+    public enum eCardSuit
+    {
+        None,
+        Heart,   //♥️
+        Diamond, //♦️
+        Clover,  //♣️
+        Pike,    //♠️
+    }
+
+
+    public class Card
+    {
+        private eCardSuit suit;
+        private int number;
+
+        public Card(eCardSuit suit, int number) 
+        {
+            this.suit = suit;
+            this.number = number;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
