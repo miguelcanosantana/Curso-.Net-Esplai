3f3a5c1fd093bc849130252974a3fc8e23685804
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-02 14:50:53 +0200

    Update Program.cs

--- 1ºMes/EjerciciosC#/CardsBattleGame/Program.cs
+++ 1ºMes/EjerciciosC#/CardsBattleGame/Program.cs
@@ -33,7 +33,55 @@ namespace CardsBattleGame
 
     public class Deck
     {
-        public Deck() { }
+        private List<Card> cards = new List<Card> ();
+
+
+        public Deck() 
+        {
+            GenerateDeck();
+        }
+
+
+        private void GenerateDeck()
+        {
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for (int n = 0; n < 13; n++)
+                {
+                    Card newCard;
+
+                    switch (suit)
+                    {
+                        case 0:
+                            cards.Add(new Card(eCardSuit.Heart, n));
+                            break;
+
+                        case 1:
+                            cards.Add(new Card(eCardSuit.Diamond, n));
+                            break;
+
+                        case 2:
+                            cards.Add(new Card(eCardSuit.Clover, n));
+                            break;
+
+                        case 3:
+                            cards.Add(new Card(eCardSuit.Diamond, n));
+                            break;
+                    }
+                }
+            }
+        }
+
+
+        public void Shuffle()
+        {
+
+        }
+
+        public void Steal()
+        {
+
+        }
     }
 
 
