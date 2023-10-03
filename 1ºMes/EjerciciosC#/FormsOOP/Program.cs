49e2bba5e125b5c54592c384585221b990a57fae
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-03 19:15:49 +0200

    Update Program.cs

--- 1ºMes/EjerciciosC#/FormsOOP/Program.cs
+++ 1ºMes/EjerciciosC#/FormsOOP/Program.cs
@@ -44,7 +44,7 @@ namespace FormsOOP
     public class Rectangle : Polygon
     {
 
-        private Rectangle(float width, float height) : base (width, height)
+        public Rectangle(float width, float height) : base (width, height)
         {
             this.height = height;
             this.width = width;
@@ -59,7 +59,8 @@ namespace FormsOOP
 
     public class Square : Rectangle
     {
-        public Square(float width) : base(width) 
+        //Using width width in the base
+        public Square(float width) : base(width, width) 
         {
 
         }
@@ -87,6 +88,73 @@ namespace FormsOOP
     {
         static void Main(string[] args)
         {
+            CalculateArea();
+            Console.ReadKey();
+        }
+
+
+        static private void CalculateArea()
+        {
+            Console.WriteLine("Choose a figure type:");
+            Console.WriteLine("1.Circle");
+            Console.WriteLine("2.Rectangle");
+            Console.WriteLine("3.Square");
+            Console.WriteLine("4.Triangle");
+            Console.Write("Input: ");
+
+            int result = int.Parse(Console.ReadLine());
+            float width;
+            float height;
+            float radius;
+            
+
+            switch (result)
+            {
+                case 1:
+                    Console.Write("Enter radius: ");
+                    radius = float.Parse(Console.ReadLine());
+
+                    Circle circle = new Circle(radius);
+                    Console.WriteLine(circle.Area());
+
+                    break;
+
+                case 2:
+
+                    Console.Write("Enter width: ");
+                    width = float.Parse(Console.ReadLine());
+
+                    Console.Write("Enter height: ");
+                    height = float.Parse(Console.ReadLine());
+
+                    Rectangle rectangle = new Rectangle(width, height);
+                    Console.WriteLine(rectangle.Area());
+
+                    break;
+
+                case 3:
+
+                    Console.Write("Enter width: ");
+                    width = float.Parse(Console.ReadLine());
+
+                    Square square = new Square(width);
+                    Console.WriteLine(square.Area());
+
+                    break;
+
+                case 4:
+
+                    Console.Write("Enter width: ");
+                    width = float.Parse(Console.ReadLine());
+
+                    Console.Write("Enter height: ");
+                    height = float.Parse(Console.ReadLine());
+
+                    Triangle triangle = new Triangle(width, height);
+                    Console.WriteLine(triangle.Area());
+
+                    break;
+            }
         }
     }
 }
