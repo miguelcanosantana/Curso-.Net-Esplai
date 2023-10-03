e4ea713bdaf650b786d84d4ef64d3b8d57966aea
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-03 18:33:10 +0200

    Added

---
+++ 1ºMes/EjerciciosC#/FormsOOP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsOOP
+{
+    public class Form
+    {
+        public Form() { }
+    }
+
+
+    public class Circle : Form
+    {
+        public Circle() { }
+    }
+
+
+    public class Polygon : Form
+    {
+        public Polygon() { }
+    }
+
+
+    public class Rectangle : Polygon
+    {
+        public Rectangle() { }
+    }
+
+
+    public class Square : Rectangle
+    {
+        public Square() { }
+    }
+
+
+    public class Triangle: Polygon
+    {
+        public Triangle() { }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
