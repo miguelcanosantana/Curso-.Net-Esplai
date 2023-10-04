ef48abf6f377ee804dd62f8762f49a9d0df2a597
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-04 18:19:19 +0200

    Estructura base.

---
+++ 1ºMes/EjerciciosC#/HospitalProject/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalProject
+{
+    public class Person
+    {
+        string id;
+        string name;
+        string surname;
+        Hospital assignedHospital;
+
+        public Person() { }
+    }
+}
