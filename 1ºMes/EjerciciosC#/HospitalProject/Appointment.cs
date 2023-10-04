ce84a385e1d51cbf66224e394de33e492236b27e
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-04 18:46:09 +0200

    Actualizado.

--- 1ºMes/EjerciciosC#/HospitalProject/Appointment.cs
+++ 1ºMes/EjerciciosC#/HospitalProject/Appointment.cs
@@ -14,6 +14,19 @@ namespace HospitalProject
         Medic medic;
         Patient patient;
 
-        public Appointment(string id, string summary, string date, Medic medic, Patient patient) { }
+        public Appointment(string id, string summary, string date, Medic medic, Patient patient) 
+        {
+            this.id = id;
+            this.summary = summary;
+            this.date = date;
+            this.medic = medic;
+            this.patient = patient;
+        }
+
+        public string GetId() { return id; }
+        public string GetSummary() { return summary; }
+        public string GetDate() { return date; }
+        public Medic GetMedic() {  return medic; }
+        public Patient GetPatient() {  return patient; }
     }
 }
--- 1ºMes/EjerciciosC#/HospitalProject/Hospital.cs
+++ 1ºMes/EjerciciosC#/HospitalProject/Hospital.cs
@@ -13,8 +13,43 @@ namespace HospitalProject
         string location;
         List<Appointment> appointmentsList = new List<Appointment>();
         List<Medic> medicsList = new List<Medic>();
-        List<Patient> patientList = new List<Patient>();
+        List<Patient> patientsList = new List<Patient>();
 
-        public Hospital() { }
+        public Hospital(string id, string name, string location) 
+        {
+            this.id = id;
+            this.name = name;
+            this.location = location;
+        }
+
+        public void AddAppointment(Appointment appointment)
+        {
+            appointmentsList.Add(appointment);
+        }
+
+        public void AddMedic(Medic medic)
+        {
+            medicsList.Add(medic);
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            patientsList.Add(patient);
+        }
+
+        public List<Appointment> FindInAppointments(Patient patient)
+        {
+            return this.appointmentsList.FindAll(appointment => appointment.GetPatient().Equals(patient));
+        }
+
+        public List<Appointment> FindInAppointments(Medic medic)
+        {
+            return this.appointmentsList.FindAll(appointment => appointment.GetPatient().Equals(medic));
+        }
+
+        public void CreateAppointment(string id, string summary, string date, Medic medic, Patient patient)
+        {
+            Appointment newAppointment = new Appointment(id, summary, date, medic, patient);
+        }
     }
 }
