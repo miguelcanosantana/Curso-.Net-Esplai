984e021e8503b1905532d561abdf2d683d21a3e0
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-05 18:09:00 +0200

    Refactor.

---
+++ 1ºMes/EjerciciosC#/SnakeClasses/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeClasses
+{
+    public class GameManager
+    {
+        private Grid grid { get; set; }
+        private Snake snake { get; set; }
+        private Apple Apple { get; set; }
+    }
+}
