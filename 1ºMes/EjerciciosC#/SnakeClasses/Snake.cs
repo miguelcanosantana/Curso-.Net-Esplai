1766e84cad3a4d6bc3c94eb5c994de7bc49cb922
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-05 18:19:44 +0200

    Update Snake.cs

--- 1ºMes/EjerciciosC#/SnakeClasses/Snake.cs
+++ 1ºMes/EjerciciosC#/SnakeClasses/Snake.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,9 @@ namespace SnakeClasses
 {
     public class Snake
     {
+        private List<Vector2> snakeParts = new List<Vector2>();
+        private List<Vector2> previousPartsPositions = new List<Vector2>();
+        private Vector2 inputDirection = Vector2.Zero;
+        private ConsoleKey previousKey;
     }
 }
