06db6539bc8dcd37c1ecdd5b515c63f30d75f8d1
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-11 19:36:28 +0200

    Create SQLQuery.sql

---
+++ 2ºMes/Employees/SQLQuery.sql
@@ -0,0 +1,33 @@
+USE MiguelCanoEmployees
+GO
+
+
+--Listar empleados que empiezan por S y tengán salario superior a X
+--Listar empleados con sueldo igual o superior a X
+SELECT first_name, last_name FROM employees
+WHERE first_name LIKE 'S%' AND salary >= 10000
+
+
+--Listar empleados cuyo nombre acaba por S
+SELECT first_name, last_name FROM employees
+WHERE first_name LIKE '%S'
+
+
+--Listar empleados cuyo nombre tiene S con 4 caracteres detrás
+SELECT first_name, last_name FROM employees
+WHERE first_name LIKE 'S____'
+
+
+--Listar empleados cuyo nombre empieza por S
+SELECT first_name, last_name FROM employees
+WHERE first_name LIKE 'S%'
+
+
+--Listar empleados con sueldo igual o superior a X
+SELECT first_name, last_name FROM employees
+WHERE salary >= 13000
+
+--Listar empleados del departamento de Marketing
+SELECT first_name, last_name FROM employees
+WHERE department_id = 2
+
