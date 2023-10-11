760bdb84ab55561f4b0ed432be912c84b3b791a4
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-11 18:30:39 +0200

    Create Query Insertar datos Base Datos Limpieza.sql

---
+++ 2ºMes/Query Insertar datos Base Datos Limpieza.sql
@@ -0,0 +1,6 @@
+USE MiguelCanoLimpieza
+GO
+
+INSERT INTO Cliente
+VALUES ('99999999S','Miguel Cano','Programador'), ('99996666S','Pepa','Dueña de una fábrica de Donuts'), ('555556666S','Pepe','Dueña de un hospital de ballenas')
+SELECT * FROM Cliente
