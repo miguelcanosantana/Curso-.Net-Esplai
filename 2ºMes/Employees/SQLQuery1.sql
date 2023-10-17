64e3039277384c8b7750767cfcae5c9e976e4d75
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-17 15:30:54 +0200

    Update SQLQuery1.sql

--- 2ºMes/Employees/SQLQuery1.sql
+++ 2ºMes/Employees/SQLQuery1.sql
@@ -9,7 +9,7 @@ WHERE YEAR (hire_date) = 1995
 
 --If the month is a multiple of 3, the module must be 0
 SELECT * FROM [dbo].[employees]
-WHERE MONTH (hire_date) % 3 = 0
+WHERE MONTH (hire_date) % 3 != 0
 
 SELECT * FROM [dbo].[employees]
 WHERE YEAR (GETDATE()) - YEAR (hire_date) >= 25
