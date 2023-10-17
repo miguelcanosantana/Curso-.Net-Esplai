7e94dc8ad021bccf9ca641f410643f9103608114
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-17 15:41:29 +0200

    Update SQLQuery1.sql

--- 2ºMes/Employees/SQLQuery1.sql
+++ 2ºMes/Employees/SQLQuery1.sql
@@ -8,8 +8,9 @@ SELECT * FROM [dbo].[employees]
 WHERE YEAR (hire_date) = 1995
 
 --If the month is a multiple of 3, the module must be 0
+--The module of a smaller number than it's divisor always returns the same number
 SELECT * FROM [dbo].[employees]
-WHERE MONTH (hire_date) % 3 != 0
+WHERE MONTH (hire_date) % 3 = 1
 
 SELECT * FROM [dbo].[employees]
 WHERE YEAR (GETDATE()) - YEAR (hire_date) >= 25
