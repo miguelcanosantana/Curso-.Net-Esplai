3d1f764566959896b620b36a1f2117f23788ee0f
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-17 15:29:29 +0200

    Update SQLQuery1.sql

--- 2ºMes/Employees/SQLQuery1.sql
+++ 2ºMes/Employees/SQLQuery1.sql
@@ -3,6 +3,13 @@ USE MiguelCanoEmployees
 SELECT * FROM [dbo].[employees]
 WHERE first_name LIKE 'S%' AND salary >= 5000 AND salary <= 10000
 
---Using Year before a date to extract only the YEAR of the entire date
+--Using Year before a date to extract only the year of the entire date
 SELECT * FROM [dbo].[employees]
 WHERE YEAR (hire_date) = 1995
+
+--If the month is a multiple of 3, the module must be 0
+SELECT * FROM [dbo].[employees]
+WHERE MONTH (hire_date) % 3 = 0
+
+SELECT * FROM [dbo].[employees]
+WHERE YEAR (GETDATE()) - YEAR (hire_date) >= 25
