6b28f2dba0d3f7f38559330d07004033234464f5
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-23 17:08:39 +0200

    Update StartingFunctions.sql

--- 2ºMes/StartingFunctions.sql
+++ 2ºMes/StartingFunctions.sql
@@ -4,9 +4,7 @@ ALTER FUNCTION TodaysDate
 --Parameters go here
 )
 RETURNS DATE
-
 AS
-
 BEGIN
 	DECLARE @resultado DATE; --Variable
 	SET @resultado = GETDATE(); --Asignar valor a variable
@@ -17,3 +15,20 @@ GO --Use go to execute the next code in a next transaction
 
 --Use Function
 SELECT dbo.TodaysDate()
+
+
+CREATE FUNCTION TodaysDay
+(
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @resultado INT;
+	SET @resultado = DAY(GETDATE());
+	RETURN @resultado;
+END
+
+GO 
+
+--Use Function
+SELECT dbo.TodaysDay()
