f8cfb6a69ba03453a1cecd8523950b0de839d106
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-23 18:44:00 +0200

    Update Funciones.sql

--- 2ºMes/Employees/Funciones.sql
+++ 2ºMes/Employees/Funciones.sql
@@ -23,3 +23,33 @@ SELECT dbo.EmployeesNumberFromDepartment(1)
 
 -- 01- Lista de depts con el num. de sus emps (usandoa la función 00)
 SELECT dbo.EmployeesNumberFromDepartment(department_id) FROM departments
+
+
+-- 1- Función NombreDepartamento
+-- recibe idDept, devuelve Nombre del dept
+CREATE FUNCTION DepartmentName(@idDept INT) 
+RETURNS VARCHAR(30)
+AS
+BEGIN
+	DECLARE @departmentName VARCHAR(30)
+
+	SELECT @departmentName = department_name FROM departments
+	WHERE department_id = @idDept
+
+	return @departmentName
+END
+
+GO 
+
+--Use Function
+SELECT dbo.DepartmentName(1)
+
+ 
+
+-- 2- Función NumEmpsPosteriorA <fecha contratación>
+-- recibe fechaContratación, devuelve num de emps
+
+ 
+
+-- 3- Función NumEmpsCiudad 
+-- recibe nombre ciudad, devuelve num de emps en esta ciudad
