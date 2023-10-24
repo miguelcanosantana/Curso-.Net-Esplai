ffa50f981cb20f9c62b446cded066c48f0732278
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-24 19:15:33 +0200

    Update Insert.sql

--- 2ºMes/Employees/Insert.sql
+++ 2ºMes/Employees/Insert.sql
@@ -11,3 +11,15 @@ INSERT INTO dependents (first_name, last_name, relationship, employee_id) VALUES
 
 SELECT * FROM dependents
 WHERE employee_id = 126
+
+
+--Increment a 10% of IT salary
+UPDATE employees SET salary = salary + (salary / 10) FROM employees
+	INNER JOIN departments
+	ON departments.department_id = employees.department_id
+WHERE department_name = 'IT'
+
+SELECT salary FROM employees
+	INNER JOIN departments
+	ON departments.department_id = employees.department_id
+WHERE department_name = 'IT'
