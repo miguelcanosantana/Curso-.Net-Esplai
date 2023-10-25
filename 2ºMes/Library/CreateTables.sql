ef0700a44ddfbc8fa08ecc71d4e389a37cbfcae5
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-25 16:25:05 +0200

    Update CreateTables.sql

--- 2ºMes/Library/CreateTables.sql
+++ 2ºMes/Library/CreateTables.sql
@@ -1,3 +1,11 @@
 USE MiguelCanoLibrary
 GO
 
+CREATE TABLE Socio (
+	IdSocio INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	CodigoSocio CHAR(15) NOT NULL,
+	DNISocio CHAR(9) NOT NULL,
+	TelefonoSocio VARCHAR(15) NOT NULL,
+	NombreSocio VARCHAR(50) NOT NULL,
+	ApellidosSocio VARCHAR(100) NOT NULL
+);
