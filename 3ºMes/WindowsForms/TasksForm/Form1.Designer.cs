acf3487307b26b48551d7cd7d2593dc6afef1209
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-27 17:43:58 +0200

    Update Form1.Designer.cs

--- 3ºMes/WindowsForms/TasksForm/Form1.Designer.cs
+++ 3ºMes/WindowsForms/TasksForm/Form1.Designer.cs
@@ -30,55 +30,308 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.locationComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.typeComboBox = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.criticityComboBox = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.environmentCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.statusComboBox = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.percentCompleteComboBox = new System.Windows.Forms.ComboBox();
+            this.mailCheckBox = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.Size = new System.Drawing.Size(161, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Create new Task";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(16, 62);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 16);
+            this.label2.Size = new System.Drawing.Size(38, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Title";
             // 
-            // textBox1
+            // titleTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(19, 81);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(162, 22);
-            this.textBox1.TabIndex = 2;
+            this.titleTextBox.Location = new System.Drawing.Point(19, 81);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(262, 22);
+            this.titleTextBox.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(217, 62);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(298, 62);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.Size = new System.Drawing.Size(66, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "Location";
+            // 
+            // locationComboBox
+            // 
+            this.locationComboBox.FormattingEnabled = true;
+            this.locationComboBox.Location = new System.Drawing.Point(301, 81);
+            this.locationComboBox.Name = "locationComboBox";
+            this.locationComboBox.Size = new System.Drawing.Size(248, 24);
+            this.locationComboBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Type";
+            // 
+            // typeComboBox
+            // 
+            this.typeComboBox.FormattingEnabled = true;
+            this.typeComboBox.Items.AddRange(new object[] {
+            "Incident",
+            "Advert",
+            "Work"});
+            this.typeComboBox.Location = new System.Drawing.Point(19, 155);
+            this.typeComboBox.Name = "typeComboBox";
+            this.typeComboBox.Size = new System.Drawing.Size(162, 24);
+            this.typeComboBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(198, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Criticity";
+            // 
+            // criticityComboBox
+            // 
+            this.criticityComboBox.FormattingEnabled = true;
+            this.criticityComboBox.Items.AddRange(new object[] {
+            "Minor",
+            "Mid",
+            "Major",
+            "Deadly",
+            "Black Hole"});
+            this.criticityComboBox.Location = new System.Drawing.Point(201, 155);
+            this.criticityComboBox.Name = "criticityComboBox";
+            this.criticityComboBox.Size = new System.Drawing.Size(162, 24);
+            this.criticityComboBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(380, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Environment";
+            // 
+            // environmentCheckedListBox
+            // 
+            this.environmentCheckedListBox.FormattingEnabled = true;
+            this.environmentCheckedListBox.Items.AddRange(new object[] {
+            "Prod",
+            "Proprod",
+            "Demo"});
+            this.environmentCheckedListBox.Location = new System.Drawing.Point(383, 155);
+            this.environmentCheckedListBox.Name = "environmentCheckedListBox";
+            this.environmentCheckedListBox.Size = new System.Drawing.Size(166, 55);
+            this.environmentCheckedListBox.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 224);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Description";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 243);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(533, 120);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(16, 383);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Start date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(19, 402);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(267, 22);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(299, 383);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 16);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Duration (In hours)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(16, 461);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 16);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Status";
+            // 
+            // statusComboBox
+            // 
+            this.statusComboBox.FormattingEnabled = true;
+            this.statusComboBox.Items.AddRange(new object[] {
+            "Planned",
+            "Not Planned",
+            "Working on it",
+            "Cancelled"});
+            this.statusComboBox.Location = new System.Drawing.Point(19, 480);
+            this.statusComboBox.Name = "statusComboBox";
+            this.statusComboBox.Size = new System.Drawing.Size(267, 24);
+            this.statusComboBox.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(299, 461);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(130, 16);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Percent Complete";
+            // 
+            // percentCompleteComboBox
+            // 
+            this.percentCompleteComboBox.FormattingEnabled = true;
+            this.percentCompleteComboBox.Items.AddRange(new object[] {
+            "0%",
+            "25%",
+            "50%",
+            "75%",
+            "100%"});
+            this.percentCompleteComboBox.Location = new System.Drawing.Point(302, 480);
+            this.percentCompleteComboBox.Name = "percentCompleteComboBox";
+            this.percentCompleteComboBox.Size = new System.Drawing.Size(247, 24);
+            this.percentCompleteComboBox.TabIndex = 19;
+            // 
+            // mailCheckBox
+            // 
+            this.mailCheckBox.AutoSize = true;
+            this.mailCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mailCheckBox.Location = new System.Drawing.Point(19, 521);
+            this.mailCheckBox.Name = "mailCheckBox";
+            this.mailCheckBox.Size = new System.Drawing.Size(301, 20);
+            this.mailCheckBox.TabIndex = 20;
+            this.mailCheckBox.Text = "Check here if you want to send an email";
+            this.mailCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(302, 402);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(247, 22);
+            this.numericUpDown1.TabIndex = 21;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(19, 624);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(114, 23);
+            this.cancelButton.TabIndex = 22;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(435, 624);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(114, 23);
+            this.submitButton.TabIndex = 23;
+            this.submitButton.Text = "GO!";
+            this.submitButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 659);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(561, 659);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.mailCheckBox);
+            this.Controls.Add(this.percentCompleteComboBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.statusComboBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.environmentCheckedListBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.criticityComboBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.typeComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.locationComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.titleTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,8 +341,28 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox titleTextBox;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox locationComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox typeComboBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox criticityComboBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckedListBox environmentCheckedListBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox statusComboBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox percentCompleteComboBox;
+        private System.Windows.Forms.CheckBox mailCheckBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button submitButton;
     }
 }
 
