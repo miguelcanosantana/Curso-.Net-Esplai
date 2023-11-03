defdf67dcb1b46999bbf5ac32d4d4cdf1d0f6b94
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-03 16:24:41 +0100

    Update.

---
+++ 3ºMes/WindowsForms/JobsDBWinforms/Model/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsDBWinforms.Model
+{
+    public class Job
+    {
+        int JobId { get; set; }
+        String JobTitle { get; set; }
+        float MinSalary { get; set; }
+        float MaxSalary { get; set; }
+    }
+}
