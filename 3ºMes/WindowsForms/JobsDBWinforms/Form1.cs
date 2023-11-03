39d8d98f1a09daa86b57da204528edafaec42be8
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-03 17:20:54 +0100

    Added things.

--- 3ºMes/WindowsForms/JobsDBWinforms/Form1.cs
+++ 3ºMes/WindowsForms/JobsDBWinforms/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,85 @@ namespace JobsDBWinforms
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection connection;
+
         public Form1()
         {
             InitializeComponent();
+            UpdateButtons();
+        }
+
+        private void connectToDBButton_Click(object sender, EventArgs e)
+        {
+            TryConnectDB();
+            TryInsertJob("Candy Tester", 2000f, 5000f);
+        }
+
+
+        private void UpdateButtons()
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                closeDBButton.Enabled = true;
+                connectToDBButton.Enabled = false;
+            }
+            else
+            {
+                closeDBButton.Enabled = false;
+                connectToDBButton.Enabled = true;
+            }
+        }
+
+
+        private void TryConnectDB()
+        {
+            try
+            {
+                connection = new SqlConnection(
+                    @"Data source = 79.143.90.12,54321;
+                    Initial Catalog = MiguelCanoEmployees;
+                    Persist Security Info = true;
+                    User Id = sa;
+                    Password = 123456789");
+
+                connection.Open();
+                statusLabel.Text = "Connected to DB";
+            }
+            catch (Exception e)
+            {
+                statusLabel.Text = "";
+                MessageBox.Show("Error connecting to DB: " + e.Message.ToString());
+            }
+
+            UpdateButtons();
+        }
+
+
+        private void TryInsertJob(String jobName, float minSalary, float maxSalary)
+        {
+            try
+            {
+                string query = $"INSERT INTO JOBS (job_title, min_salary, max_salary) VALUES (" +
+                   "'" + jobName + "'" + "," + minSalary + "," + maxSalary + ")";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                statusLabel.Text = "Job inserted";
+            }
+            catch (Exception e)
+            {
+                statusLabel.Text = "";
+                MessageBox.Show("Error inserting Job to DB: " + e.Message.ToString());
+            }
+        }
+
+
+        private void closeDBButton_Click(object sender, EventArgs e)
+        {
+            connection.Close();
+            statusLabel.Text = "";
+            UpdateButtons();
         }
     }
 }
--- 3ºMes/WindowsForms/JobsDBWinforms/Model/Job.cs
+++ 3ºMes/WindowsForms/JobsDBWinforms/Model/Job.cs
@@ -8,9 +8,16 @@ namespace JobsDBWinforms.Model
 {
     public class Job
     {
-        int JobId { get; set; }
+        int? JobId { get; set; }
         String JobTitle { get; set; }
         float MinSalary { get; set; }
         float MaxSalary { get; set; }
+
+        public Job(String jobTitle, float minSalary, float maxSalary) 
+        {
+            JobTitle = jobTitle;
+            MinSalary = minSalary;
+            MaxSalary = maxSalary;
+        }
     }
 }
