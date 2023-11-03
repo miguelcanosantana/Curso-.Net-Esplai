a5cb1f2f7ea0e8bcf90bd367d8de4a4c95b86486
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-03 17:08:24 +0100

    Update.

---
+++ 3ºMes/WindowsForms/DBConnection/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectToDBButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.closeDBButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connectToDBButton
+            // 
+            this.connectToDBButton.Location = new System.Drawing.Point(648, 397);
+            this.connectToDBButton.Name = "connectToDBButton";
+            this.connectToDBButton.Size = new System.Drawing.Size(140, 25);
+            this.connectToDBButton.TabIndex = 0;
+            this.connectToDBButton.Text = "Connect To DB";
+            this.connectToDBButton.UseVisualStyleBackColor = true;
+            this.connectToDBButton.Click += new System.EventHandler(this.connectToDBButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(9, 9);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(91, 16);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "                            ";
+            // 
+            // closeDBButton
+            // 
+            this.closeDBButton.Location = new System.Drawing.Point(12, 397);
+            this.closeDBButton.Name = "closeDBButton";
+            this.closeDBButton.Size = new System.Drawing.Size(140, 25);
+            this.closeDBButton.TabIndex = 2;
+            this.closeDBButton.Text = "Close Connection";
+            this.closeDBButton.UseVisualStyleBackColor = true;
+            this.closeDBButton.Click += new System.EventHandler(this.closeDBButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.closeDBButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.connectToDBButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectToDBButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button closeDBButton;
+    }
+}
+
---
+++ 3ºMes/WindowsForms/DBConnection/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateButtons();
+        }
+
+        private void connectToDBButton_Click(object sender, EventArgs e)
+        {
+            TryConnectDB();
+        }
+
+
+        private void UpdateButtons()
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                closeDBButton.Enabled = true;
+                connectToDBButton.Enabled = false;
+            }
+            else
+            {
+                closeDBButton.Enabled = false;
+                connectToDBButton.Enabled = true;
+            }
+        }
+
+
+        private void TryConnectDB()
+        {
+            try
+            {
+                connection = new SqlConnection(
+                    @"Data source = 79.143.90.12,54321;
+                    Initial Catalog = MiguelCanoEmployees;
+                    Persist Security Info = true;
+                    User Id = sa;
+                    Password = 123456789");
+
+                connection.Open();
+                statusLabel.Text = "Connected to DB";
+            }
+            catch (Exception e)
+            {
+                statusLabel.Text = "";
+                MessageBox.Show("Error connecting to DB: " + e.Message.ToString());
+            }
+
+            UpdateButtons();
+        }
+
+
+        private void TryInsertJob(String jobName, float minSalary, float maxSalary)
+        {
+            try
+            {
+                string query = $"INSERT INTO JOBS (job_title, min_salary, max_salary) VALUES (" +
+                    jobName + "," + minSalary + "," + maxSalary + ")";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                statusLabel.Text = "Job inserted";
+            }
+            catch (Exception e)
+            {
+                statusLabel.Text = "";
+                MessageBox.Show("Error inserting Job to DB: " + e.Message.ToString());
+            }
+        }
+
+
+        private void closeDBButton_Click(object sender, EventArgs e)
+        {
+            connection.Close();
+            statusLabel.Text = "";
+            UpdateButtons();
+        }
+    }
+}
