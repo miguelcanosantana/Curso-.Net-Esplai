c07b3b14797546d6de290b0a46617d48ed2985b9
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-08 16:01:03 +0100

    Update.

--- 3ºMes/WindowsForms/EmployeesLinq/Form1.Designer.cs
+++ 3ºMes/WindowsForms/EmployeesLinq/Form1.Designer.cs
@@ -29,25 +29,90 @@
         private void InitializeComponent()
         {
             this.employeesListBox = new System.Windows.Forms.ListBox();
+            this.getAllButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // employeesListBox
             // 
             this.employeesListBox.FormattingEnabled = true;
             this.employeesListBox.ItemHeight = 16;
-            this.employeesListBox.Location = new System.Drawing.Point(12, 22);
+            this.employeesListBox.Location = new System.Drawing.Point(12, 12);
             this.employeesListBox.Name = "employeesListBox";
-            this.employeesListBox.Size = new System.Drawing.Size(385, 404);
+            this.employeesListBox.Size = new System.Drawing.Size(606, 404);
             this.employeesListBox.TabIndex = 0;
+            // 
+            // getAllButton
+            // 
+            this.getAllButton.Location = new System.Drawing.Point(624, 12);
+            this.getAllButton.Name = "getAllButton";
+            this.getAllButton.Size = new System.Drawing.Size(126, 23);
+            this.getAllButton.TabIndex = 1;
+            this.getAllButton.Text = "Get all employees";
+            this.getAllButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 433);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(606, 230);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create Employee";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surnames";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 22);
+            this.textBox2.TabIndex = 3;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(879, 675);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.getAllButton);
             this.Controls.Add(this.employeesListBox);
             this.Name = "MainForm";
             this.Text = "Employees";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +120,12 @@
         #endregion
 
         private System.Windows.Forms.ListBox employeesListBox;
+        private System.Windows.Forms.Button getAllButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- 3ºMes/WindowsForms/EmployeesLinq/Program.cs
+++ 3ºMes/WindowsForms/EmployeesLinq/Program.cs
@@ -8,6 +8,9 @@ namespace EmployeesLinq
 {
     internal static class Program
     {
+        static DataClasses1DataContext db = new DataClasses1DataContext();
+        static List<employees> employeesList = new List<employees>();
+
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
@@ -17,6 +20,45 @@ namespace EmployeesLinq
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+
+        private static bool TryInsertEmployee(
+            String name, 
+            String lastName, 
+            String mail, 
+            String phone,
+            int jobId,
+            decimal money,
+            int? managerId,
+            int? departmentId
+            )
+        {
+            try
+            {
+                employees emp = new employees
+                {
+                   first_name = name,
+                   last_name = lastName,
+                   email = mail,
+                   phone_number = phone,
+                   hire_date = DateTime.Now,
+                   job_id = jobId,
+                   salary = money,
+                   manager_id = managerId,
+                   department_id = departmentId
+                };
+
+                db.GetTable<employees>().InsertOnSubmit(emp);
+                db.SubmitChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error inserting Employee to DB: " + e.Message.ToString());
+                return false;
+            }
         }
     }
 }
