9d2c7f432c9c13a48ed094bcfeda25453c6e1c85
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-08 16:57:37 +0100

    Update Form1.Designer.cs

--- 3ºMes/WindowsForms/EmployeesLinq/Form1.Designer.cs
+++ 3ºMes/WindowsForms/EmployeesLinq/Form1.Designer.cs
@@ -31,23 +31,24 @@
             this.employeesListBox = new System.Windows.Forms.ListBox();
             this.getAllButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cNameTextBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cSurnamesTextBox = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.cMailTextBox = new System.Windows.Forms.TextBox();
-            this.cPhoneTextBox = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.cSalaryTextBox = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.cManagerIdTextBox = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.cDepartmentIdTextBox = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.createEmployeeButton = new System.Windows.Forms.Button();
             this.cJobIdTextBox = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.createEmployeeButton = new System.Windows.Forms.Button();
+            this.cDepartmentIdTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cManagerIdTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cSalaryTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cPhoneTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cMailTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cSurnamesTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -96,101 +97,31 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Create Employee";
             // 
-            // label1
+            // cJobIdTextBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
+            this.cJobIdTextBox.Location = new System.Drawing.Point(225, 115);
+            this.cJobIdTextBox.Name = "cJobIdTextBox";
+            this.cJobIdTextBox.Size = new System.Drawing.Size(179, 22);
+            this.cJobIdTextBox.TabIndex = 16;
             // 
-            // cNameTextBox
+            // label8
             // 
-            this.cNameTextBox.Location = new System.Drawing.Point(25, 52);
-            this.cNameTextBox.Name = "cNameTextBox";
-            this.cNameTextBox.Size = new System.Drawing.Size(179, 22);
-            this.cNameTextBox.TabIndex = 1;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(222, 96);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Job Id";
             // 
-            // label2
+            // createEmployeeButton
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 96);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 16);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Surnames";
-            // 
-            // cSurnamesTextBox
-            // 
-            this.cSurnamesTextBox.Location = new System.Drawing.Point(25, 115);
-            this.cSurnamesTextBox.Name = "cSurnamesTextBox";
-            this.cSurnamesTextBox.Size = new System.Drawing.Size(179, 22);
-            this.cSurnamesTextBox.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 162);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 16);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Mail";
-            // 
-            // cMailTextBox
-            // 
-            this.cMailTextBox.Location = new System.Drawing.Point(25, 181);
-            this.cMailTextBox.Name = "cMailTextBox";
-            this.cMailTextBox.Size = new System.Drawing.Size(179, 22);
-            this.cMailTextBox.TabIndex = 5;
-            // 
-            // cPhoneTextBox
-            // 
-            this.cPhoneTextBox.Location = new System.Drawing.Point(225, 52);
-            this.cPhoneTextBox.Name = "cPhoneTextBox";
-            this.cPhoneTextBox.Size = new System.Drawing.Size(179, 22);
-            this.cPhoneTextBox.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(222, 33);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 16);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Phone";
-            // 
-            // cSalaryTextBox
-            // 
-            this.cSalaryTextBox.Location = new System.Drawing.Point(225, 181);
-            this.cSalaryTextBox.Name = "cSalaryTextBox";
-            this.cSalaryTextBox.Size = new System.Drawing.Size(179, 22);
-            this.cSalaryTextBox.TabIndex = 9;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(222, 162);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 16);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Salary";
-            // 
-            // cManagerIdTextBox
-            // 
-            this.cManagerIdTextBox.Location = new System.Drawing.Point(427, 115);
-            this.cManagerIdTextBox.Name = "cManagerIdTextBox";
-            this.cManagerIdTextBox.Size = new System.Drawing.Size(179, 22);
-            this.cManagerIdTextBox.TabIndex = 11;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(424, 96);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(136, 16);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Manager Id (Optional)";
+            this.createEmployeeButton.Location = new System.Drawing.Point(427, 162);
+            this.createEmployeeButton.Name = "createEmployeeButton";
+            this.createEmployeeButton.Size = new System.Drawing.Size(179, 41);
+            this.createEmployeeButton.TabIndex = 14;
+            this.createEmployeeButton.Text = "Submit";
+            this.createEmployeeButton.UseVisualStyleBackColor = true;
+            this.createEmployeeButton.Click += new System.EventHandler(this.createEmployeeButton_Click);
             // 
             // cDepartmentIdTextBox
             // 
@@ -208,37 +139,117 @@
             this.label7.TabIndex = 12;
             this.label7.Text = "Department Id (Optional)";
             // 
-            // createEmployeeButton
+            // cManagerIdTextBox
             // 
-            this.createEmployeeButton.Location = new System.Drawing.Point(427, 162);
-            this.createEmployeeButton.Name = "createEmployeeButton";
-            this.createEmployeeButton.Size = new System.Drawing.Size(179, 41);
-            this.createEmployeeButton.TabIndex = 14;
-            this.createEmployeeButton.Text = "Submit";
-            this.createEmployeeButton.UseVisualStyleBackColor = true;
-            this.createEmployeeButton.Click += new System.EventHandler(this.createEmployeeButton_Click);
+            this.cManagerIdTextBox.Location = new System.Drawing.Point(427, 115);
+            this.cManagerIdTextBox.Name = "cManagerIdTextBox";
+            this.cManagerIdTextBox.Size = new System.Drawing.Size(179, 22);
+            this.cManagerIdTextBox.TabIndex = 11;
             // 
-            // cJobIdTextBox
+            // label6
             // 
-            this.cJobIdTextBox.Location = new System.Drawing.Point(225, 115);
-            this.cJobIdTextBox.Name = "cJobIdTextBox";
-            this.cJobIdTextBox.Size = new System.Drawing.Size(179, 22);
-            this.cJobIdTextBox.TabIndex = 16;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(424, 96);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(136, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Manager Id (Optional)";
             // 
-            // label8
+            // cSalaryTextBox
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(222, 96);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(44, 16);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "Job Id";
+            this.cSalaryTextBox.Location = new System.Drawing.Point(225, 181);
+            this.cSalaryTextBox.Name = "cSalaryTextBox";
+            this.cSalaryTextBox.Size = new System.Drawing.Size(179, 22);
+            this.cSalaryTextBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Salary";
+            // 
+            // cPhoneTextBox
+            // 
+            this.cPhoneTextBox.Location = new System.Drawing.Point(225, 52);
+            this.cPhoneTextBox.Name = "cPhoneTextBox";
+            this.cPhoneTextBox.Size = new System.Drawing.Size(179, 22);
+            this.cPhoneTextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Phone";
+            // 
+            // cMailTextBox
+            // 
+            this.cMailTextBox.Location = new System.Drawing.Point(25, 181);
+            this.cMailTextBox.Name = "cMailTextBox";
+            this.cMailTextBox.Size = new System.Drawing.Size(179, 22);
+            this.cMailTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mail";
+            // 
+            // cSurnamesTextBox
+            // 
+            this.cSurnamesTextBox.Location = new System.Drawing.Point(25, 115);
+            this.cSurnamesTextBox.Name = "cSurnamesTextBox";
+            this.cSurnamesTextBox.Size = new System.Drawing.Size(179, 22);
+            this.cSurnamesTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surnames";
+            // 
+            // cNameTextBox
+            // 
+            this.cNameTextBox.Location = new System.Drawing.Point(25, 52);
+            this.cNameTextBox.Name = "cNameTextBox";
+            this.cNameTextBox.Size = new System.Drawing.Size(179, 22);
+            this.cNameTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(639, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(228, 621);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filter By";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(879, 675);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.getAllButton);
             this.Controls.Add(this.employeesListBox);
@@ -272,6 +283,7 @@
         private System.Windows.Forms.Button createEmployeeButton;
         private System.Windows.Forms.TextBox cJobIdTextBox;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
