da6bbccd2bda11444e6d7c3e8cb2fd0cbf37c3e6
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-16 19:23:10 +0100

    Dni.

--- 3ºMes/JS/Initial/dni.js
+++ 3ºMes/JS/Initial/dni.js
@@ -1,8 +1,29 @@
-function calculatePow() {
-    var dniValue = document.getElementById('dniInput').value
+function validateDNI() {
 
+    var isValid = false
 
-    
-    // var result = Math.pow(numberValue, powValue)
-    // document.getElementById("result-p").innerHTML = "Result: " + result;
+    try {
+
+        var dniValue = document.getElementById('dniInput').value
+
+        var numbers = dniValue.substr(0, 8);
+        console.log(numbers)
+
+        var module = numbers % 23
+        console.log(module)
+
+        var letters = "TRWAGMYFPDXBNJZSQVHLCKE"
+        var letterResult = letters[module]
+        console.log(letterResult)
+        console.log(dniValue[8])
+
+        if (dniValue[8] == letterResult) {
+            isValid = true
+        }
+        
+    } catch (error) {
+        
+    }
+
+    document.getElementById("result-p").innerHTML = "Is valid: " + isValid;
 }
