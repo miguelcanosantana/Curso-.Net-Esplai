6ebd0da0b196ee2393a519b79858ce8562e2a2c0
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-16 19:10:50 +0100

    DNI

---
+++ 3ºMes/JS/Initial/dni.js
@@ -0,0 +1,8 @@
+function calculatePow() {
+    var dniValue = document.getElementById('dniInput').value
+
+
+    
+    // var result = Math.pow(numberValue, powValue)
+    // document.getElementById("result-p").innerHTML = "Result: " + result;
+}
