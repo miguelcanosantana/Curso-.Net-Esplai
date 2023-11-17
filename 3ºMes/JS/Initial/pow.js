7ebded3e3940567b9f25b1c95c0c87b21f9e9853
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-17 16:25:14 +0100

    Update pow.js

--- 3ºMes/JS/Initial/pow.js
+++ 3ºMes/JS/Initial/pow.js
@@ -1,7 +1,13 @@
 
 function calculatePow() {
     var numberValue = document.getElementById('numberInput').value
-    var powValue = document.getElementById('powInput').value
-    var result = Math.pow(numberValue, powValue)
+    var exponentValue = document.getElementById('powInput').value
+
+    var result = 1
+
+    for (i = 0; i < exponentValue; i++) {
+        result *= numberValue;
+    }
+
     document.getElementById("result-p").innerHTML = "Result: " + result;
 }
