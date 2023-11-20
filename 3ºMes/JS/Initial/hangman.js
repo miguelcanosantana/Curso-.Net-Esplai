d075c90e66d3955712aefe3861a86445d49d6419
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-20 19:31:36 +0100

    Update.

--- 3ºMes/JS/Initial/hangman.js
+++ 3ºMes/JS/Initial/hangman.js
@@ -88,35 +88,3 @@ function tryGuessWord(word) {
     }
     isMatchEnd = true
 }
-
-
-
-function validateDNI() {
-
-    var isValid = false
-
-    try {
-
-        var dniValue = document.getElementById('dniInput').value
-
-        var numbers = dniValue.substr(0, 8);
-        console.log(numbers)
-
-        var module = numbers % 23
-        console.log(module)
-
-        var letters = "TRWAGMYFPDXBNJZSQVHLCKE"
-        var letterResult = letters[module]
-        console.log(letterResult)
-        console.log(dniValue[8])
-
-        if (dniValue[8] == letterResult) {
-            isValid = true
-        }
-        
-    } catch (error) {
-        
-    }
-
-    document.getElementById("result-p").innerHTML = "Is valid: " + isValid;
-}
---
+++ 3ºMes/JS/TicTacToe/tictac.js
@@ -0,0 +1,32 @@
+var currentPlayer = Math.round(Math.random())
+var currentPlayerName = null
+
+updateCurrentPlayer(false)
+
+
+//Update current player turn
+function updateCurrentPlayer(isChangingPlayer) {
+
+    if (isChangingPlayer == true){
+
+        if (currentPlayer == 0) {
+            currentPlayer = 1
+        }
+        else {
+            currentPlayer = 0
+        }
+    }
+
+    if (currentPlayer == 0) {
+        currentPlayerName = "X"
+    } else {
+        currentPlayerName = "O"
+    }
+
+    document.getElementById("current-player-text").innerHTML = currentPlayerName + "'s round."
+}
+
+
+function clickCell(cellId) {
+    console.log(cellId)
+}
