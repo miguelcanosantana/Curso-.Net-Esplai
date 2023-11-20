f75847771309c6b12fa5f9c491da737cda51c84a
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-20 15:53:06 +0100

    Update.

--- 3ºMes/JS/multiple.js
+++ 3ºMes/JS/multiple.js
@@ -3,3 +3,15 @@ function changeColor(buttonId, color) {
     var button = document.getElementById(buttonId)
     button.style.backgroundColor = color
 }
+
+
+function changeColorOfElement(color) {
+
+    if(document.getElementById('entire-doc-radio').checked) {
+        document.body.style.backgroundColor = color
+
+    } else if(document.getElementById('layer-radio').checked) {
+        document.getElementById('small-layer').style.backgroundColor = color
+    }
+
+}
