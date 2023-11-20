21a411b1a5f8e21244fe310555cda03a5643a3a1
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-20 16:08:59 +0100

    Update.

--- 3ºMes/JS/multiple.js
+++ 3ºMes/JS/multiple.js
@@ -13,5 +13,26 @@ function changeColorOfElement(color) {
     } else if(document.getElementById('layer-radio').checked) {
         document.getElementById('small-layer').style.backgroundColor = color
     }
+}
 
+
+function showAlertAge() {
+
+    let name = prompt("Enter your name:");
+
+    if (name == null || name == "") {
+        window.alert("Name was left empty");
+        return
+    }
+
+    let age = prompt("Enter your age:");
+
+    if (age == null || age == "" || age < 0) {
+        window.alert("Age was not correct");
+        return
+    }
+
+    document.getElementById("name-result-prompt").innerHTML = "Name: " + name;
+    document.getElementById("age-result-prompt").innerHTML = "Age: " + age;
+    document.getElementById("lived-result-prompt").innerHTML = "Lived: " + age * 365 + " days";
 }
