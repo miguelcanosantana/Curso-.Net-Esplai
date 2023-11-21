36fd2b1e30062924419279cb85180457286e1b06
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-21 15:53:49 +0100

    Update tictac.js

--- 3ºMes/JS/TicTacToe/tictac.js
+++ 3ºMes/JS/TicTacToe/tictac.js
@@ -39,6 +39,54 @@ function clickCell(cellId) {
 
     if (cell.innerHTML != "X" && cell.innerHTML != "O") {
         cell.innerHTML = currentPlayerName
+
+        var winner = checkWinner(currentPlayerName)
+
+        //TODO
+        //if (winner)
+
         updateCurrentPlayer(true)
     }
+}
+
+
+function checkWinner(symbol) {
+
+    //Horizontal checks
+    if (document.getElementById("cell-1").innerHTML == symbol &&
+        document.getElementById("cell-2").innerHTML == symbol &&
+        document.getElementById("cell-3").innerHTML == symbol ||
+        document.getElementById("cell-4").innerHTML == symbol &&
+        document.getElementById("cell-5").innerHTML == symbol &&
+        document.getElementById("cell-6").innerHTML == symbol ||
+        document.getElementById("cell-7").innerHTML == symbol &&
+        document.getElementById("cell-8").innerHTML == symbol &&
+        document.getElementById("cell-9").innerHTML == symbol) {
+            return symbol
+    }
+
+    //Vertical checks
+    if (document.getElementById("cell-1").innerHTML == symbol &&
+        document.getElementById("cell-4").innerHTML == symbol &&
+        document.getElementById("cell-7").innerHTML == symbol ||
+        document.getElementById("cell-2").innerHTML == symbol &&
+        document.getElementById("cell-5").innerHTML == symbol &&
+        document.getElementById("cell-8").innerHTML == symbol ||
+        document.getElementById("cell-3").innerHTML == symbol &&
+        document.getElementById("cell-6").innerHTML == symbol &&
+        document.getElementById("cell-9").innerHTML == symbol) {
+            return symbol
+    }
+
+    //Diagonal checks
+    if (document.getElementById("cell-1").innerHTML == symbol &&
+        document.getElementById("cell-5").innerHTML == symbol &&
+        document.getElementById("cell-9").innerHTML == symbol ||
+        document.getElementById("cell-7").innerHTML == symbol &&
+        document.getElementById("cell-5").innerHTML == symbol &&
+        document.getElementById("cell-3").innerHTML == symbol) {
+            return symbol
+    }
+
+    return null
 }
