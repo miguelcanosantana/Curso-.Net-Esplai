bfd18c674a2a13b28a36531d0ae1f2e836ca8f04
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-28 15:56:11 +0100

    Added.

--- 3ºMes/ASPNET/MVC/WebApplicationInitial/Controllers/HomeController.cs
+++ 3ºMes/ASPNET/MVC/WebApplicationInitial/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace WebApplicationInitial.Controllers
             return View();
         }
 
+        public IActionResult FirstView()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
