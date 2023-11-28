631f22eb0e4f1e652c5dac68f787c21c84595d70
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-28 19:33:23 +0100

    Create AnimalsTypeInteractiveViewComponent.cs

---
+++ 3ºMes/ASPNET/MVC/WebApplicationInitial/ViewComponents/AnimalsTypeInteractiveViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationInitial.ViewComponents
+{
+    public class AnimalsTypeInteractiveViewComponent: ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            Dictionary<string, string> animalTypes = new Dictionary<string, string>()
+            {
+                {"Fish", "Fish are super decorative"},
+                {"Elephants", "Elephants are big sized mammals with long trumps"}
+            };
+
+            return View(animalTypes);
+        }
+    }
+}
