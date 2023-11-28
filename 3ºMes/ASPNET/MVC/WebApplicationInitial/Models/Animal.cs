b76fcb48aa1779ff7589145c54cfd8c942b0c051
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-28 18:17:26 +0100

    Update Animal.cs

--- 3ºMes/ASPNET/MVC/WebApplicationInitial/Models/Animal.cs
+++ 3ºMes/ASPNET/MVC/WebApplicationInitial/Models/Animal.cs
@@ -1,8 +1,16 @@
-﻿namespace WebApplicationInitial.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationInitial.Models
 {
 	public class Animal
 	{
+		[Required]
+		[Range(0, 50, ErrorMessage = "The value for {0} must be between {1} and {2}")]
 		public int id { get; set; }
+
+		[Required]
+		[Display(Name = "Animal")]
+		[StringLength(15, ErrorMessage = "The name can't exceed 15 characters")]
 		public String name { get; set; }
 
 		public Animal(int id, String name) {
