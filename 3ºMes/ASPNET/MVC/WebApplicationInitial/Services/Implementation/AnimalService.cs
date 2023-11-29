fcdd0bd645d66c245b16bb56c59b8b42f75714a5
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-29 18:42:44 +0100

    Update.

---
+++ 3ºMes/ASPNET/MVC/WebApplicationInitial/Services/Implementation/AnimalService.cs
@@ -0,0 +1,18 @@
+﻿using WebApplicationInitial.Models;
+using WebApplicationInitial.Services.Contract;
+
+namespace WebApplicationInitial.Services.Implementation
+{
+	public class AnimalService : IAnimalService
+	{
+		public Task<List<Animal>> GetAnimals()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task InsertAnimal(Animal animal)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
