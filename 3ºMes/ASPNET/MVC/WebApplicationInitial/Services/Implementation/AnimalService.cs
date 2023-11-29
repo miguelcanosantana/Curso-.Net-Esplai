8ad6991545cb41c1f636b19b0173001367f9f913
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-29 18:45:01 +0100

    Update AnimalService.cs

--- 3ºMes/ASPNET/MVC/WebApplicationInitial/Services/Implementation/AnimalService.cs
+++ 3ºMes/ASPNET/MVC/WebApplicationInitial/Services/Implementation/AnimalService.cs
@@ -1,10 +1,20 @@
-﻿using WebApplicationInitial.Models;
+﻿using Microsoft.Extensions.Options;
+using WebApplicationInitial.Models;
+using WebApplicationInitial.Services.Config;
 using WebApplicationInitial.Services.Contract;
 
 namespace WebApplicationInitial.Services.Implementation
 {
 	public class AnimalService : IAnimalService
 	{
+		private readonly ConnectionConfig connetion;
+
+		public AnimalService(IOptions<ConnectionConfig> connetion)
+		{
+			this.connetion = connetion.Value;
+		}
+
+
 		public Task<List<Animal>> GetAnimals()
 		{
 			throw new NotImplementedException();
