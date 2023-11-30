9ee313a1f6da0af8c9a044290ed7ff99c1f3c4d7
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-11-30 15:49:40 +0100

    Update.

---
+++ 3ºMes/ASPNET/Apis/WebApiTest/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTest.Models
+{
+	public class Animal
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public string breed { get; set; }
+		public int? fkAnimalType { get; set; }
+		public DateTime bornDate { get; set; }
+	}
+
+}
